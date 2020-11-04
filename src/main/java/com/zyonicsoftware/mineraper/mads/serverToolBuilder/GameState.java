b27325cfa91fe701d383a5f846e09e218637566b
AzraAnimating <azraanimating@gmail.com>
2020-11-05 00:15:17 +0100

    added GameState

---
+++ src/main/java/com/zyonicsoftware/mineraper/mads/serverToolBuilder/GameState.java
@@ -0,0 +1,16 @@
+/*
+ * 2020. Zyonic Software - Tobias Rempe
+ * This File, its contents and by extention the corresponding project may be used in compliance with the attached license.
+ *
+ * Minecraft Application Development System is developed by Tobias Rempe for MineReaperDE
+ *
+ * tobiasrempe@zyonicsoftware.com
+ */
+
+package com.zyonicsoftware.mineraper.mads.serverToolBuilder;
+
+public enum GameState {
+    LOBBY,
+    INGAME,
+    END
+}
