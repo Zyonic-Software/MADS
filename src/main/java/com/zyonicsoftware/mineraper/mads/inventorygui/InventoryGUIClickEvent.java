72efd81360282cc71c5b09107392eaeaf20de6f9
Daschi1 <50054971+Daschi1@users.noreply.github.com>
2020-12-25 14:59:46 +0100

    Added an custom wrapped InventoryClickEvent (InventoryGUIClickEvent)

---
+++ src/main/java/com/zyonicsoftware/mineraper/mads/inventorygui/InventoryGUIClickEvent.java
@@ -0,0 +1,13 @@
+package com.zyonicsoftware.mineraper.mads.inventorygui;
+
+import org.bukkit.event.inventory.InventoryClickEvent;
+
+public class InventoryGUIClickEvent extends InventoryClickEvent {
+
+    /**
+     * @param inventoryClickEvent The {@link InventoryClickEvent} to be wrapped
+     */
+    public InventoryGUIClickEvent(final InventoryClickEvent inventoryClickEvent) {
+        super(inventoryClickEvent.getView(), inventoryClickEvent.getSlotType(), inventoryClickEvent.getSlot(), inventoryClickEvent.getClick(), inventoryClickEvent.getAction(), inventoryClickEvent.getHotbarButton());
+    }
+}
