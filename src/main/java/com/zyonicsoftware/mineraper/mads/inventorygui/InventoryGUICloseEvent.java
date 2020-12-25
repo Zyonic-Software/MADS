38e12e2cf0f7f30f03b253a604c2b244d0595a13
Daschi1 <50054971+Daschi1@users.noreply.github.com>
2020-12-25 15:00:03 +0100

    Added an custom wrapped InventoryCloseEvent (InventoryGUICloseEvent)

---
+++ src/main/java/com/zyonicsoftware/mineraper/mads/inventorygui/InventoryGUICloseEvent.java
@@ -0,0 +1,38 @@
+package com.zyonicsoftware.mineraper.mads.inventorygui;
+
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+
+public class InventoryGUICloseEvent extends InventoryCloseEvent implements Cancellable {
+
+    /**
+     * If the {@link InventoryCloseEvent} should be cancelled
+     *
+     * @see Cancellable
+     */
+    private boolean cancelled;
+
+    /**
+     * @param inventoryCloseEvent The {@link InventoryCloseEvent} to be wrapped
+     */
+    public InventoryGUICloseEvent(final InventoryCloseEvent inventoryCloseEvent) {
+        super(inventoryCloseEvent.getView(), inventoryCloseEvent.getReason());
+        this.cancelled = false;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean isCancelled() {
+        return this.cancelled;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void setCancelled(final boolean cancel) {
+        this.cancelled = cancel;
+    }
+}
